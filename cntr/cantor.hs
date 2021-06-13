e6be4a3aa00d47626163d3082bb0c1ce54b2bf00
takumi4162 <matsuwaka@DESKTOP-ALOIR3Q.localdomain>
2021-06-14 03:29:50 +0900

    add tst

--- cntr/cantor.hs
+++ cntr/cantor.hs
@@ -7,6 +7,11 @@ cantor x n
   | 2/3 <= x && x <= 1 = 1/2 + 1/2 * (cantor (3*x-2) (n-1))
   | otherwise = x
 
+-- test case --
+cantor_test1 = cantor 0.20 100 == 0.25
+cantor_test2 = cantor 0.25 100 == 1/3
+cantor_test3 = cantor (200/243) 100 == 0.75
+
 main = do 
   let n = 100   -- precision of cantor function
   x <- getLine
